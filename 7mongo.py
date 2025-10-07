a1a069a704fcfc352b780faa9160660e1ffad5e0
KL09 JOKER <105102757+KL09JOKER@users.noreply.github.com>
2025-10-07 08:05:47 +0530

    Create 7mongo.py

---
+++ 7mongo.py
@@ -0,0 +1,25 @@
+Id:
+Objectid C68 C92 C7 e 18 F C 5 1 2 C 2 5 8 6 42526′)
+title:
+shaller me
+genre: Romance
+Sales: 45600
+availability:
+549
+_id: ObjectHd C 68c965 81 2 2 6 18 2 1 6 2 1 3 6 12 8 4 86') 286182162136 84
+Litle:
+Richdad Poordad
+benre: Sci-fic
+Sales: 49608
+availability:
+662
+-Id: Objeckla L' 6 3 2 3 4 5 C 2 1 8 7 6 5 8422394432P12')
+the: tsitp
+benre: drama
+Sales: 780
+availability: 12
+- 10: ObjectId('13682345CC 21876 5842e3q4зер63')
+title: All the bright places.
+benre: Sci-Hic
+Sales: 40
+avallability: 2
