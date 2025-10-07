1cf52a4ee7dcc05cd8124f8b2971dfe31bb7fb67
KL09 JOKER <105102757+KL09JOKER@users.noreply.github.com>
2025-10-07 08:06:27 +0530

    Create 8mongo.py

---
+++ 8mongo.py
@@ -0,0 +1,20 @@
+id: Objectid ('686957671780865106968008')
+CUSTOMER ID : " CUSTOOL"
+METER ID: "MIROOI"
+NAME: "lemy"
+_id: Objectid ('68ca57671780825106968008)
+meler la boog"
+reading value. 13442
+reading dale: 2025-07 00710:00:002
+id: Objechid (1680e70c06cb98cd062851')
+meler ra: "MROOI"
+reading valle 2025-07-31T00:00:00+000+00:00
+reading value: 12350
+-Id: objectia b8c1432106PCd062858)
+meler-10 MTROO2"
+preading value: 1500 ale (2025-06-30100 00:0023"
+reading-dala:"1500"
+Id: Objectid ('68cces818680304bd1516)
+meler.id "MTROO3"
+reading dala: 1500 ate ( 2025-06-30:00:00:003)"
+reading value: "1600
